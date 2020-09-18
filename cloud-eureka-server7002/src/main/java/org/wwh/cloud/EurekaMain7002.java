1a1a853990e41b8dd7593e35f97b5a4a9f42215d
WangWeihao <wangweihao@mysteel.com>
2020-09-18 11:38:21 +0800

    删除不需要的文件

---
+++ cloud-eureka-server7002/src/main/java/org/wwh/cloud/EurekaMain7002.java
@@ -0,0 +1,14 @@
+package org.wwh.cloud;
+
+/**
+ * @author wwh
+ * @date 2020/9/18 10:33
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002
+{
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
