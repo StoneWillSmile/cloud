7cc53c394f128a66a975427768ea4cdf80b5689c
WangWeihao <wangweihao@mysteel.com>
2020-10-21 09:20:12 +0800

    feat dubbo test

---
+++ dubbo-test/src/main/java/org/wwh/dubbo/DubboMain.java
@@ -0,0 +1,15 @@
+package org.wwh.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wwh
+ * @date 2020/10/16 14:22
+ */
+@SpringBootApplication
+public class DubboMain {
+    public static void main(String[] args) {
+        SpringApplication.run(DubboMain.class, args);
+    }
+}
---
+++ dubbo-test/src/main/java/org/wwh/dubbo/service/DubboService.java
@@ -0,0 +1,8 @@
+package org.wwh.dubbo.service;
+
+/**
+ * @author wwh
+ * @date 2020/10/16 11:56
+ */
+public class DubboService {
+}
