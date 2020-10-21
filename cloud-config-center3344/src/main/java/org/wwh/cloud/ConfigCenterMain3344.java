3fbaf18bd7df1cd89443264b0c68143e7cff0b90
WangWeihao <wangweihao@mysteel.com>
2020-10-21 09:20:56 +0800

    feat spring-cloud-config

---
+++ cloud-config-center3344/src/main/java/org/wwh/cloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package org.wwh.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author wwh
+ * @date 2020/10/20 19:10
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
