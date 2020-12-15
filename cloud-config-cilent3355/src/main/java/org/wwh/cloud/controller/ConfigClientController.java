a72be26a4fd736d61ff43a02364be4943e676158
WangWeihao <wangweihao@mysteel.com>
2020-12-15 19:22:22 +0800

    feat cloud-config客户端

---
+++ cloud-config-cilent3355/src/main/java/org/wwh/cloud/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package org.wwh.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wwh
+ * @date 2020/10/24 14:21
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
